cacab491a434c94f2fc8c2b330fd83cad1e973ce
subrotomojumder <97802263+subrotomojumder@users.noreply.github.com>
2022-08-22 18:06:42 +0600

    user inter pase complete of assignment five

--- js/main.js
+++ js/main.js
@@ -1,1 +1,25 @@
-console.log('chose player added')
+// player name to show in selection list
+const playersName = [];
+
+const playerSelectionElement =  document.getElementById('select-player');
+function showSelectionList(namesArray) {
+    playerSelectionElement.innerHTML = '';
+
+    for (const name of namesArray) {
+        const nameList = name;
+        const li = document.createElement('li');
+        li.classList.add('h5')
+        li.innerText = nameList;
+        playerSelectionElement.appendChild(li);
+    }
+}
+
+function addBtn(event) {
+    const playerName = event.target.parentNode.children[0].innerText;
+    playersName.push(playerName);
+
+    showSelectionList(playersName)
+}
+
+
+
